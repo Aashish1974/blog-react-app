b1c6a4d027fdcb1887f44ffde57bd816a6b69267
Aashish1974 <aashishofficial2021@gmail.com>
2023-04-23 22:39:19 +0530

    react project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Profile from './profile';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>function Called</h1>
+        <Profile />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const myfun=function Profile()
+{
+    return (
+    <div>  
+      <h1> hello from profile function</h1> 
+    </div>
+    )
+}
+export default myfun;
+
